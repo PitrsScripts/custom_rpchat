edd9ceca206384cb23ecd3f7b0fe99d744285995
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2025-05-02 00:46:56 +0200

    Add files via upload

--- rpchat/client/main.lua
+++ rpchat/client/main.lua
@@ -50,8 +50,10 @@ RegisterNetEvent('rpchat:sendMe', function(playerId, title, message, color)
         local sourceCoords, targetCoords = GetEntityCoords(sourcePed), GetEntityCoords(targetPed)
         if targetPed == source or #(sourceCoords - targetCoords) < 20 then
             local isInVehicle = IsPedInAnyVehicle(sourcePed, false)
-            local baseOffset = isInVehicle and 0.5 or 1.0 
-            local offset = baseOffset + textOffset 
+            local baseOffset = isInVehicle and 0.7 or 1.2 
+            local textOffset = 0.0 
+            local offset = baseOffset + textOffset
+
             Citizen.CreateThread(function()
                 local startTime = GetGameTimer()
                 local displayDuration = 5000
@@ -59,7 +61,7 @@ RegisterNetEvent('rpchat:sendMe', function(playerId, title, message, color)
                     Citizen.Wait(0)
                     local targetPedCoords = GetEntityCoords(targetPed)
                     local x, y, z = targetPedCoords.x, targetPedCoords.y, targetPedCoords.z
-                    DrawText3D(x, y, z, message, {0, 151, 255, 255}, offset) 
+                    DrawText3D(x, y, z, message, {0, 151, 255, 255}, offset)
                     if GetGameTimer() - startTime > displayDuration then
                         break
                     end
@@ -70,8 +72,9 @@ RegisterNetEvent('rpchat:sendMe', function(playerId, title, message, color)
                 template = '<div style="margin-bottom: 5px; padding: 10px; background-color: rgba(10, 10, 10, 0.7); border-radius: 0px; color: white;"> <span style="background-color: rgba(0, 151, 255, 0.4); border-radius: 0px; padding: 2px 4px;">ME</span> ' .. playerName .. ' - <span style="color: white;">'.. message ..'</span></div>',
                 args = { "[ME] - " .. playerName, message }
             })
-            TriggerServerEvent('rpchat:sendToDiscord', "me", playerName .. "- " .. message, 3447003) 
-            textOffset = textOffset + 0.3 
+
+            TriggerServerEvent('rpchat:sendToDiscord', "me", playerName .. " - " .. message, 3447003)
+            textOffset = textOffset + 0.3
         end
     end
 end)
@@ -86,8 +89,10 @@ RegisterNetEvent('rpchat:sendDo', function(playerId, title, message, color)
         local sourceCoords, targetCoords = GetEntityCoords(sourcePed), GetEntityCoords(targetPed)
         if targetPed == source or #(sourceCoords - targetCoords) < 20 then
             local isInVehicle = IsPedInAnyVehicle(sourcePed, false)
-            local baseOffset = isInVehicle and 0.5 or 1.0 
-            local offset = baseOffset + textOffset 
+            local baseOffset = isInVehicle and 0.7 or 1.2 -- výš nad hlavou
+            local textOffset = 0.0 -- inicializace
+            local offset = baseOffset + textOffset
+
             Citizen.CreateThread(function()
                 local startTime = GetGameTimer()
                 local displayDuration = 5000
@@ -105,18 +110,18 @@ RegisterNetEvent('rpchat:sendDo', function(playerId, title, message, color)
                     end
                 end
             end)
+
             local playerName = GetPlayerName(target)
             TriggerEvent('chat:addMessage', {
                 template = '<div style="margin-bottom: 5px; padding: 10px; background-color: rgba(10, 10, 10, 0.7); border-radius: 0px; color: white;"> <span style="background-color: rgba(255, 255, 0, 0.4); border-radius: 0px; padding: 2px 4px;">DO</span> ' .. playerName .. ' - <span style="color: white;">'.. message ..'</span></div>',
                 args = { "[DO] - " .. playerName, message }
             })
-            TriggerServerEvent('rpchat:sendToDiscord', "do", playerName .. "- " .. message, 16776960) 
+            TriggerServerEvent('rpchat:sendToDiscord', "do", playerName .. " - " .. message, 16776960)
 
             textOffset = textOffset + 0.3
         end
     end
 end)
-
 function DrawText3D(x, y, z, text, color, offset)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z + offset)
     if onScreen then
@@ -203,19 +208,21 @@ RegisterCommand('doc', function(source, args, rawCommand)
             local textLength = string.len(text)
             local bgWidth = 0.02 + (textLength / 250)
             local bgHeight = 0.027
-            local bgX = _x
-            local bgY = _y + (bgHeight / 2) 
-            DrawRect(bgX, bgY, bgWidth, bgHeight, 0, 0, 0, 150) 
-            SetTextScale(0.35, 0.35) 
-            SetTextFont(16) 
+    
+            local offsetX = 0.005 -- posun doprava
+    
+            SetTextScale(0.35, 0.35)
+            SetTextFont(16)
             SetTextProportional(1)
-            SetTextColour(color[1], color[2], color[3], color[4]) 
+            SetTextColour(color[1], color[2], color[3], color[4])
             SetTextEntry("STRING")
-            SetTextCentre(1) 
+            SetTextCentre(1)
             AddTextComponentString(text)
-            DrawText(_x, _y)
+            DrawText(_x + offsetX, _y)
+    
+            DrawRect(_x + offsetX, _y + 0.012, bgWidth, bgHeight, 0, 0, 0, 150)
         end
-    end
+    end    
     Citizen.CreateThread(function()
         local startTime = GetGameTimer()
         local updateInterval = 1000
@@ -223,25 +230,24 @@ RegisterCommand('doc', function(source, args, rawCommand)
 
         while count <= target do
             Citizen.Wait(0)
-
+        
             local targetCoords = GetEntityCoords(targetPed)
-            local x, y, z = targetCoords.x, targetCoords.y, targetCoords.z + 1.0
-
+            local x, y, z = targetCoords.x, targetCoords.y, targetCoords.z + 1.2 -- zvýšená výška
+        
             local currentTime = GetGameTimer()
             if currentTime - lastUpdate >= updateInterval then
                 lastUpdate = currentTime
                 count = count + 1
             end
             DrawText3D(x, y, z, "" .. count .. " / " .. target, {255, 255, 255, 255}) 
-
+        
             if count > target then
                 break
             end
-        end
+        end        
     end)
     TriggerServerEvent('rpchat:sendToDiscord', "doc", "Příkaz vykonán s cílem: " .. target, 16777215) 
 end, false)
-
 --------------------------------
 --------------ADMIN-------------
 --------------------------------
--- stavzde/fxmanifest.lua
+++ stavzde/fxmanifest.lua
@@ -1,7 +1,5 @@
 game 'gta5'
 fx_version 'cerulean'
 
-lua54 'yes' 
-
 client_script 'client.lua'
 server_script 'server.lua'
