79cce68d70ae2ac7d2c6e61e5d554020ff752a5c
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2025-06-17 18:00:53 +0200

    Add files via upload

---
+++ rpchat/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Pitrs'
+description 'ESX RPCHAT'
+version '1.0'
+
+lua54 'yes' 
+
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua',
+	'@ox_lib/init.lua', 
+}
+
+server_scripts {
+	'@async/async.lua',
+	'server/*.lua',
+}
+
+client_scripts {
+	'client/*.lua',
+}
+
+
+
+
